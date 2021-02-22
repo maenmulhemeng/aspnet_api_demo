f7d4e0f87fbd90024ea9903b732b61fd40140b59
maenmulhem <maen@gta-canada.com>
2021-02-22 15:57:41 +0300

    Solution was prepared

---
+++ VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace maen_vog_backend_codechallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : Controller
+    {
+       
+        [HttpGet]
+        public IActionResult Index()
+        {
+         
+            return Ok("initialized");
+        }
+       
+    }
+}
